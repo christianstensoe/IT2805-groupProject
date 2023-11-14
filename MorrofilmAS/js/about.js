90f8356c12d451adc54e8081fdb6489cb229fbef
Hermanlous <hermanrorholtlous@gmail.com>
2023-11-14 10:02:19 +0100

    lagt inn enkelte kommentarer

--- MorrofilmAS/js/about.js
+++ MorrofilmAS/js/about.js
@@ -11,16 +11,23 @@ function selectPerson(ID){
     }
 }
 
-function getOut(ID) {
-    event.stopImmediatePropagation()
-    let out = ID.replace('button', '');
-    let allElements = document.querySelectorAll('.personObject');
+document.querySelectorAll('button').forEach(function(button){
+    button.addEventListener('click', function(event){
+        getOut(button.id,event);
+    })
+})
+
+function getOut(ID,event) {
+    if(event){event.stopImmediatePropagation()//sjekker her om dersom det er 'click', og hvis eventet er et klikk hindrer den andre eventlisteners (som hører etter 'click') fra å kalles
+    }
+    let out = ID.replace('button', ''); //lager en variabel som erstatter "button" med en tom streng, slik at knappens ID = section ID 
+    let allElements = document.querySelectorAll('.personObject'); // lager her en variabel som inneholder alle elementene i classen personObject
 
     allElements.forEach(function(element) {
-        if (element.id == out) {
-            element.classList.remove('active');
+        if (element.id == out) { //sjekker om elementets ID tilsvarer knappens ID
+            element.classList.remove('active');//dersom elementets ID tilsvarer ID-en til knappen skal den fjerne classen 'active'
         } else {
-            element.classList.remove('hidden');
+            element.classList.remove('hidden');// ellers skal den fjerne classen 'hidden'
         }
     });
 }
